9bffcd4c592d3f993e519c3b702b620851597381
Joseph (YOLK) Chiocchi <input.output@gmail.com>
2012-05-04 06:16:19 -0500

    jekyll'd

--- _site/js/wordflash.js
+++ _site/js/wordflash.js
@@ -7,18 +7,21 @@ Description:
     Takes some text, and flashes one word at a time to the screen.
 */
 
-var text = $('#ccontent').text();
+var text = $("#ccontent").text();
 var countmax;
 var c=0;
-
-wordr();
 
 function wordr(){
     splt = text.split(" ");
     countmax = splt.length;
-    $('#wordflash').text(splt[c]);
+    $("#wordflash").text(splt[c]);
     c=c+1;
     if (c>countmax)
         c=0;
     setTimeout("wordr()", 250);
+
+
 }
+
+
+wordr();
