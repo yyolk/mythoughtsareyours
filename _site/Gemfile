bfb25c25fff2135b51b5882b56d4a317ab82f447
Joseph Yølk Chiocchi <joe@yolk.cc>
2014-10-16 13:21:16 -0500

    update styles

--- _site/Gemfile
+++ _site/Gemfile
@@ -1,5 +1,6 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem "compass"
 gem "compass-blueprint"
 gem "jekyll"
--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-$(function(){
-    spectrum();
+// $(function(){
+//     spectrum();
 
-    function spectrum(){
-        var colr = Math.floor(Math.random()*256);
-        var icolr = 255 - colr >> 0;
-        var hue = 'rgb(' + colr + ',' + colr + ',' + colr + ')';
-            var ihue = 'rgb(' + icolr + ',' + icolr + ',' + icolr +  ')';
-            $('body').animate( { backgroundColor: hue }, 3000);
-            //$('header').animate( { color: ihue } , 3000);
-            $('#YOLK, .post, header').animate( { color: ihue }, 3000);
-            $('#YOLK, .post, header').animate( {textShadow: 'rgba('+colr+','+colr+','+colr+',0.8) 0 0 14px;'}, 3000);
-            setTimeout(function(){},1000);
-            spectrum();
-            }
+//     function spectrum(){
+//         var colr = Math.floor(Math.random()*256);
+//         var icolr = 255 - colr >> 0;
+//         var hue = 'rgb(' + colr + ',' + colr + ',' + colr + ')';
+//             var ihue = 'rgb(' + icolr + ',' + icolr + ',' + icolr +  ')';
+//             $('body').animate( { backgroundColor: hue }, 3000);
+//             //$('header').animate( { color: ihue } , 3000);
+//             $('#YOLK, .post, header').animate( { color: ihue }, 3000);
+//             $('#YOLK, .post, header').animate( {textShadow: 'rgba('+colr+','+colr+','+colr+',0.8) 0 0 14px;'}, 3000);
+//             setTimeout(function(){},1000);
+//             spectrum();
+//             }
             
-            });
+//             });
 
 
